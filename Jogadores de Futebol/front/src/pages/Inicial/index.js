58ea076b64ba14d6a4955f9beb322a0547e70c2a
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-05-26 08:31:44 -0300

    Add files via upload

---
+++ Jogadores de Futebol/front/src/pages/Inicial/index.js
@@ -0,0 +1,13 @@
+// front\src\pages\Inicial\index.js
+
+import PaginaInicial from "../../components/PaginaInicial";
+import './styles.css';
+
+function Inicial() {
+    return (
+        <div className='pagina-inicial'>
+            <PaginaInicial />
+        </div>
+    );
+}
+export default Inicial;
